49afc7326a57a398ce36db08d690ac316d9e225f
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-06-02 11:27:01 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  output: {
+    filename: "[name].pack.js",
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env", "babel-preset-react"],
+          },
+        },
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  entry: {
+    index: "./src/index.js",
+  },
+};
